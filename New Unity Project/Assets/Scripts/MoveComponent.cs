9eaff20e9e28831d173aaa91fe2c3d7b57d06317
Eric <ninuson123@gmail.com>
2016-02-17 03:33:15 -0700

    Fixed the way detectors work. Added a few examples of collider set ups and fixed a few bugs with movement.

---
+++ New Unity Project/Assets/Scripts/MoveComponent.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveComponent : MonoBehaviour 
+{
+    public Rigidbody2D rigid_body;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+    public int FORCE_CONST = 150;
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rigid_body.AddForce(transform.up * FORCE_CONST);
+        } 
+        if (Input.GetKey(KeyCode.DownArrow ))
+        {
+            rigid_body.AddForce(-transform.up * FORCE_CONST);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigid_body.MoveRotation(rigid_body.rotation + 5.0f);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigid_body.MoveRotation(rigid_body.rotation - 5.0f);
+        }
+	}
+}
