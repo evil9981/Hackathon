3dd9d4d51557b0c59e12d92e44db7ba6b9df4e94
roger <Roger.sun.2008@gmail.com>
2016-02-17 15:18:47 -0700

    End of Game menus.

--- New Unity Project/Assets/Scripts/InGameMenu.cs
+++ New Unity Project/Assets/Scripts/InGameMenu.cs
@@ -24,6 +24,7 @@ public class InGameMenu : MonoBehaviour {
         {
             if (ingameMenu.enabled == false)
             {
+
                 ingameMenu.enabled = true;
             }
             else
