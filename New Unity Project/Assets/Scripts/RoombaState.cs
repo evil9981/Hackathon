31dcd28aec96a0c5d6d111afa893fa4d67d0ea2d
Eric_eidl <ninuson123@gmail.com>
2016-02-17 17:17:31 -0700

    Added RoombaStatus.

---
+++ New Unity Project/Assets/Scripts/RoombaState.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoombaState : MonoBehaviour 
+{
+    decimal battery_level = 100.0M;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
