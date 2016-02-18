58becff0d53391f21318dc749cff15435f86961a
roger <Roger.sun.2008@gmail.com>
2016-02-17 19:45:36 -0700

    Display scores and shit

---
+++ New Unity Project/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLogic : MonoBehaviour {
+
+    int collectedTrash = 0;
+    public int passCondition;
+    public int totalPass;
+    public displayCollected display;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+
+    public void trashCollected()
+    {
+        collectedTrash += 1;
+        display.change_to_collected(collectedTrash);
+    }
+
+    public int getCollected()
+    {
+        return collectedTrash;
+    }
+
+
+}
---
+++ New Unity Project/Assets/Scripts/displayCollected.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class displayCollected : MonoBehaviour {
+
+    public GameLogic gameLogic;
+    public Sprite normal;
+    public Sprite gold;
+    public float x;
+    public float y;
+    public float scale;
+    public int normalTrash;
+    public int BonusTrash;
+    public GameObject gray;
+    int collected;
+
+    List<GameObject> collected_display = new List<GameObject>();
+
+    // Use this for initialization
+    void Start () {
+        int totalTrash = normalTrash + BonusTrash;
+        
+        for (int i = 0; i < totalTrash; i++) {
+            float spacing = 9f / (totalTrash - 1f);
+            Vector3 pos = new Vector3(x + spacing * i, y, -2f);
+            GameObject temp = (GameObject)Instantiate(gray, transform.position, transform.rotation);
+            temp.transform.parent = gameObject.transform;
+            temp.transform.position = pos;
+            temp.transform.localScale = new Vector3(scale, scale, scale);
+            collected_display.Add(temp);
+        }
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+    public void change_to_collected(int num_collected)
+    {
+        if (num_collected < normalTrash)
+        {
+            collected_display[num_collected - 1].GetComponent<SpriteRenderer>().sprite = normal;
+        }
+        else
+        {
+            collected_display[num_collected - 1].GetComponent<SpriteRenderer>().sprite = gold;
+        }
+    }
+
+}
