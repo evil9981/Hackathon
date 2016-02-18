2527cea2f894fbcd3bfedf347e6fb4ed857e551e
JohannWentzel <wentzel142@gmail.com>
2016-02-18 11:53:24 -0700

    Changed around levels

--- New Unity Project/Assets/Scripts/displayCollected.cs
+++ New Unity Project/Assets/Scripts/displayCollected.cs
@@ -27,7 +27,8 @@ public class DisplayCollected : MonoBehaviour
 
         for (int i = 0; i < totalTrash; i++)
         {
-            float spacing = 90f / (totalTrash - 1f);
+            float spacing = 9f / (totalTrash - 1f);
+            Vector3 pos = new Vector3(x + spacing * i, y, 0.4f);
 
             GameObject temp;
             if (i < normalTrash)
@@ -38,9 +39,9 @@ public class DisplayCollected : MonoBehaviour
             {
                 temp = (GameObject)Instantiate(gold, transform.position, transform.rotation);
             }
-
             temp.transform.parent = gameObject.transform;
-            temp.transform.localPosition = new Vector3(x + spacing * i, y, 0.4f);
+            temp.transform.position = pos;
+            temp.transform.localPosition = new Vector3(temp.transform.localPosition.x, temp.transform.localPosition.y, 0.4f);
             temp.transform.localScale = new Vector3(scale, scale, scale);
             collected_display.Add(temp);
         }
